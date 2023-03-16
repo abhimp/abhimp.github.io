ec3da9ee3abcd9cc233389e291d8bf2d794a8d4a
Abhijit Mondal <abhijit.manpur@gmail.com>
2023-03-17 00:09:43 +0530

    updating

--- assets/js/travel.js
+++ assets/js/travel.js
@@ -74,6 +74,12 @@ $(document).ready(function(){
             if($(ele).attr("data-caption")) {
                 imgData["splCaption"] = $(ele).attr("data-caption")
             }
+            if($(ele).attr("data-lat") && $(ele).attr("data-lng") && $(ele).attr("data-url")) {
+                imgData["latlng"] = "<a href=\""+encodeURI($(ele).attr("data-url"))+"\" target=\"_blank\">"
+                                    + "GPS: " + $(ele).attr("data-lat")+","+$(ele).attr("data-lng")
+                                    + "<\a>"
+            }
+            // console.log(imgData)
             images.push(imgData)
             $(ele).click(function(){
                 lightbox.loadAndOpen(i);
@@ -83,22 +89,7 @@ $(document).ready(function(){
     })
     lightbox.addFilter('numItems', photoSwipeNumItems);
     lightbox.addFilter('itemData', photoSwipeItemData);
-    // lightbox.on('contentActivate', ({ content }) => {
-    //     console.log('contentActivate', content);
-    //     if(!DoNotChangeHash) {
-    //         window.location.hash = content.index;
-    //         ChangedHash = true
-    //     }
-    //     DoNotChangeHash = false;
-    // });
-    // lightbox.on('close', () => {
-    //     if(!DoNotChangeHash) {
-    //         window.location.hash = ""
-    //         ChangedHash = true
-    //     }
-    //     DoNotChangeHash = false;
-    //     // window.history.pushState(window.location.href.split('#')[0]);
-    // });
+
     lightbox.on('close', () => {
         pswpContainer.style.display = 'none';
         if (fullscreenAPI && fullscreenAPI.isFullscreen()) {
@@ -119,6 +110,9 @@ $(document).ready(function(){
                     if(!captionHTML)
                         captionHTML = lightbox.pswp.currSlide.data.alt || "";
                     // captionHTML = "<h1>ASdasd</h1>";
+                    if (lightbox.pswp.currSlide.data.latlng) {
+                        captionHTML += "<br>" + lightbox.pswp.currSlide.data.latlng
+                    }
                     if(captionHTML) {
                         el.innerHTML = captionHTML;
                         $(el).css("opacity", 1)
@@ -189,74 +183,3 @@ function getContainer() {
     document.body.appendChild(pswpContainer);
     return pswpContainer;
 }
-
-// function openModal() {
-//     document.getElementById("myModal").style.display = "block";
-// }
-
-// function closeModal() {
-//     document.getElementById("myModal").style.display = "none";
-// }
-
-// // var slideIndex = 1;
-// // showSlides(slideIndex);
-
-// function plusSlides(n) {
-//     showSlides(slideIndex += n);
-// }
-
-// function currentSlide(n) {
-//     showSlides(slideIndex = n);
-// }
-
-// function showSlides(n) {
-//     var i;
-//     var slides = document.getElementsByClassName("mySlides");
-//     // var dots = document.getElementsByClassName("demo");
-//     var captionText = document.getElementById("caption");
-//     if (n > slides.length) {slideIndex = 1}
-//     if (n < 1) {slideIndex = slides.length}
-//     for (i = 0; i < slides.length; i++) {
-//         slides[i].style.display = "none";
-//     }
-//     // for (i = 0; i < dots.length; i++) {
-//     //     dots[i].className = dots[i].className.replace(" active", "");
-//     // }
-//     slides[slideIndex-1].style.display = "block";
-//     // dots[slideIndex-1].className += " active";
-//     // captionText.innerHTML = dots[slideIndex-1].alt;
-// }
-// function donothing() {
-//     return false;
-// }
-
-// $(document).ready(function(){
-//     $("img.travel").each(function(i,ele){
-//         console.log(ele)
-//         var origimg = $(ele).attr("origimg")
-//         if(origimg)
-//         {
-//             $(ele).click(function(){
-//                 openModal();
-//                 currentSlide(i+1)
-//             })
-//             var origimg = $(ele).attr("origimg")
-//             var div = $("<div></div>").addClass("travel").addClass("mySlides")
-//             var img = $("<img>").attr("src", origimg).addClass("travel").addClass("imgSlide").appendTo(div)
-//             $(".travel.modal-content").append(div)
-//         }
-//     })
-//     var prev = $("<a class=\"travel prev\" >&#10094;</a>").click(function(){plusSlides(-1)})
-//     var next = $("<a class=\"travel next\" >&#10095;</a>").click(function(){plusSlides(1)})
-//     $(".travel.modal-content").append(prev).append(next)
-//     $("#myModal").click(function(ev){
-//         if(ev.target == $("#myModal")[0] || ev.target == $("#myModalContent")[0])
-//             closeModal()
-//     })
-//     // $("#myImgModal").click(function(ev){
-//     //     console.log(ev)
-//     //     if(ev.target == $("#myImgModal")[0])
-//     //         closeImageModal()
-//     // })
-// })
-
